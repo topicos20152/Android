b51210b3cc9fae5e524ba0f24d4781037624096f
Jeffersson Galvão <jefferssondeveloper@gmail.com>
2015-11-17 10:14:18 -0300

    Criação do Drawer com as cores do sistema já com uma prévia das turmas de forma estática e com o espaço para colocar os dados do usuário

--- app/src/main/java/com/topicos/topicosandroid/MainActivity.java
+++ app/src/main/java/com/topicos/topicosandroid/MainActivity.java
@@ -1,20 +1,32 @@
 package com.topicos.topicosandroid;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.interfaces.OnCheckedChangeListener;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.SwitchDrawerItem;
+import com.mikepenz.materialdrawer.model.ToggleDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 import com.topicos.topicosandroid.dao.SubjectDao;
 import com.topicos.topicosandroid.domain.Subject;
 import com.topicos.topicosandroid.domain.User;
@@ -32,11 +44,21 @@ public class MainActivity extends AppCompatActivity {
     private Drawer drawer;
     private AccountHeader accountHeader;
 
+    private OnCheckedChangeListener onCheckedChangeListener = new OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(IDrawerItem drawerItem, CompoundButton buttonView, boolean isChecked) {
+            Toast.makeText(MainActivity.this, "onChackedChange: " + ( isChecked ? "true" : "false"), Toast.LENGTH_SHORT).show();
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Sigaa-X");
+        toolbar.setSubtitle("TaskManager");
         setSupportActionBar(toolbar);
 
         // Mocking user with the same data from api
@@ -61,9 +83,47 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        drawer = new DrawerBuilder().withActivity(this)
-                .withToolbar(toolbar)
+
+        accountHeader = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withCompactStyle(false)
+                .withSavedInstance(savedInstanceState)
+                .withThreeSmallProfileImages(false)
+                .withHeaderBackground(R.color.primary)
+                .addProfiles(
+                        new ProfileDrawerItem().withName("Jeffersson Galvão").withEmail("jefferssondeveloper@gmail.com").withIcon(R.drawable.material_drawer_circle_mask) // colocar informações do usuário
+                )
                 .build();
+
+        drawer = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withDisplayBelowStatusBar(true)
+                .withHeaderPadding(true)
+                .withActionBarDrawerToggleAnimated(true)
+                .withDrawerGravity(Gravity.LEFT)
+                .withSavedInstance(savedInstanceState)
+                .withSelectedItem(-1)
+                .withAccountHeader(accountHeader)
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        return false;
+                    }
+                })
+                .withOnDrawerItemLongClickListener(new Drawer.OnDrawerItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(View view, int position, IDrawerItem drawerItem) {
+                        Toast.makeText(MainActivity.this, "onItemLongClick: " + position, Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                })
+                .build();
+
+        drawer.addItem(new PrimaryDrawerItem().withName("Turma 1").withIcon(R.drawable.material_drawer_circle_mask));
+        drawer.addItem(new PrimaryDrawerItem().withName("Turma 2").withIcon(R.drawable.material_drawer_circle_mask));
+        drawer.addItem(new DividerDrawerItem());
+        drawer.addItem(new SwitchDrawerItem().withName("Notificação").withChecked(true).withOnCheckedChangeListener(onCheckedChangeListener));
     }
 
     @Override

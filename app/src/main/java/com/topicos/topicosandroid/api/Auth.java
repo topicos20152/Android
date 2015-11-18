99d4a0eaaf2afb9e7f32346cb91efd5536901580
Pablo Gabriel <pablo.reds@hotmail.com>
2015-11-18 19:40:22 -0300

    Creating class to access token from API #23

---
+++ app/src/main/java/com/topicos/topicosandroid/api/Auth.java
@@ -0,0 +1,57 @@
+package com.topicos.topicosandroid.api;
+
+import com.topicos.topicosandroid.domain.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Pablo Gabriel on 15/11/2015.
+ */
+public class Auth {
+
+    private String accessToken;
+    private String expireDate;
+
+    public Auth() {
+    }
+
+    private List<Map<String, String>> getCredential(User user) {
+        List<Map<String, String>> credential = null;
+        try {
+            credential = new ApiRequest("api/v1/users/" + user.getId() + "/request_access_token", "POST", keys()).execute().get();
+
+            Map<String, String> map = credential.get(0);
+
+            accessToken = map.get(ACCESS_TOKEN);
+            expireDate = map.get(EXPIRE_DATE);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return credential;
+    }
+
+    public String getAccessToken(User user) {
+        Map<String, String> map = getCredential(user).get(0);
+        return map.get(ACCESS_TOKEN);
+    }
+
+    public String getExpireDate(User user) {
+        Map<String, String> map = getCredential(user).get(0);
+        return map.get(EXPIRE_DATE);
+    }
+
+    public static List<String> keys() {
+        List<String> keys = new ArrayList<String>();
+        keys.add("value");
+        keys.add("expire_date");
+
+        return keys;
+    }
+
+    public static final String ACCESS_TOKEN = "value";
+    public static final String EXPIRE_DATE = "expire_date";
+
+}
